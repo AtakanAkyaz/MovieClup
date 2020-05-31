3ad8ece2f351b6906e2720b0e42ba6455e7274d0
AtakanAkyaz <61373558+AtakanAkyaz@users.noreply.github.com>
2020-06-01 02:57:43 +0300

    Add files via upload

---
+++ Çalışan-cloud/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_GTkkGqVJO',
+        userPoolClientId: '67cmrv55da01hrph9jfd8e85rj', 
+        region: 'us-east-2' 
+    },
+    api: {
+        invokeUrl: ' http://movieclup10.s3-website.us-east-2.amazonaws.com' ,
+    }
+};
